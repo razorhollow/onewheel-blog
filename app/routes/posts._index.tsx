226189c76b37008bacf7db1c44f8b04ed2943ec0
RazorHollow <rob@razorhollow.com>
2024-01-10 10:16:11 -0500

    add marked/markdown functionality

--- app/routes/posts._index.tsx
+++ app/routes/posts._index.tsx
@@ -2,23 +2,10 @@ import type { LoaderFunctionArgs } from '@remix-run/node';
 import { json } from '@remix-run/node';
 import { Link, useLoaderData } from '@remix-run/react';
 
-export const loader = async ({ params, request }: LoaderFunctionArgs) => {
-  const posts = [
-    {
-      slug: "my-first-post",
-      title: "My First Post!",
-    },
-    {
-      slug: "trail-riding-with-onewheel",
-      title: "Trail Riding with Onewheel",
-    },
-  ]
+import { getPosts } from '~/models/post.server';
 
-  return json({posts}, {
-    headers: {
-      'Content-Type': "application/json"
-    }
-  })
+export const loader = async () => {
+  return json({ posts: await getPosts() })
 };
 
 export default function Posts() {
---
+++ app/routes/posts.sample-post.tsx
@@ -0,0 +1,17 @@
+export default function Me() {
+  return (
+    <div className="prose">
+      <h1>Sample Post</h1>
+      <ul>
+        <li>Some point</li>
+        <li>Some point</li>
+        <li>Some point</li>
+        <li>Some point</li>
+        <li>Some point</li>
+        <li>Some point</li>
+        <li>Some point</li>
+        <li>Some point</li>
+      </ul>
+    </div>
+  );
+}
